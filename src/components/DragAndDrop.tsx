d4eaf7d775eacbcc14d981cfbc05e4a5e0e8cc9a
karta2186810 <karta2186810@gmail.com>
2021-07-21 22:11:14 +0800

    chore: 自定義封裝Drag和Drop組件

---
+++ src/components/DragAndDrop.tsx
@@ -0,0 +1,59 @@
+import {
+  Draggable,
+  DraggableProps,
+  Droppable,
+  DroppableProps,
+  DroppableProvided,
+  DroppableProvidedProps,
+} from "react-beautiful-dnd";
+import React, { ReactNode } from "react";
+
+type DropProps = Omit<DroppableProps, "children"> & { children: ReactNode };
+
+/* 將Droppable組件進行一層封裝，免去自己需要將props傳遞給children */
+export const Drop = ({ children, ...props }: DropProps) => {
+  return (
+    <Droppable {...props}>
+      {(provided) => {
+        if (React.isValidElement(children)) {
+          // 將children複製，並傳入想要添加的props
+          return React.cloneElement(children, {
+            ...provided.droppableProps,
+            ref: provided.innerRef,
+            provided,
+          });
+        }
+        return <div />;
+      }}
+    </Droppable>
+  );
+};
+
+type DropChildProps = Partial<
+  { provided: DroppableProvided } & DroppableProvidedProps
+> &
+  React.HTMLAttributes<HTMLDivElement>;
+
+// 使用forwardRef使自定義組件可以使用ref進行綁定
+export const DropChild = React.forwardRef<HTMLDivElement, DropChildProps>(
+  (props, ref) => <div ref={ref} {...props} />
+);
+
+type DragProps = Omit<DraggableProps, "children"> & { children: ReactNode };
+
+export const Drag = ({ children, ...props }: DragProps) => {
+  return (
+    <Draggable {...props}>
+      {(provided) => {
+        if (React.isValidElement(children)) {
+          return React.cloneElement(children, {
+            ...provided.draggableProps,
+            ...provided.dragHandleProps,
+            ref: provided.innerRef,
+          });
+        }
+        return <div />;
+      }}
+    </Draggable>
+  );
+};
